8d79a7ff0eaae865bb7b6395bade40a927ac04e8
EC2 Default User <ec2-user@ip-172-31-13-217.us-east-2.compute.internal>
2018-06-28 01:13:32 +0000

    Add rlp and logger package

---
+++ common/mclock/mclock.go
@@ -0,0 +1,16 @@
+// Package mclock is a wrapper for a monotonic clock source
+package mclock
+
+import (
+	"time"
+
+	"github.com/aristanetworks/goarista/monotime"
+)
+
+// AbsTime represents absolute monotonic time.
+type AbsTime time.Duration
+
+// Now returns the current absolute monotonic time.
+func Now() AbsTime {
+	return AbsTime(monotime.Now())
+}
