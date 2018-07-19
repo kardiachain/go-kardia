4f7d27eb87a2e6190c0d9b5065c3e62b47d4ec6f
nhkv <huynguyen1505@gmail.com>
2018-07-19 23:40:26 +0000

    Add first set of Kardia VM opcodes

--- vm/contract.go
+++ vm/contract.go
@@ -161,6 +161,33 @@ func (bits *bitvec) codeSegment(pos uint64) bool {
 	return ((*bits)[pos/8] & (0x80 >> (pos % 8))) == 0
 }
 
+// codeBitmap collects data locations in code.
+func codeBitmap(code []byte) bitvec {
+	// The bitmap is 4 bytes longer than necessary, in case the code
+	// ends with a PUSH32, the algorithm will push zeroes onto the
+	// bitvector outside the bounds of the actual code.
+	bits := make(bitvec, len(code)/8+1+4)
+	for pc := uint64(0); pc < uint64(len(code)); {
+		op := OpCode(code[pc])
+
+		if op >= PUSH1 && op <= PUSH32 {
+			numbits := op - PUSH1 + 1
+			pc++
+			for ; numbits >= 8; numbits -= 8 {
+				bits.set8(pc) // 8
+				pc += 8
+			}
+			for ; numbits > 0; numbits-- {
+				bits.set(pc)
+				pc++
+			}
+		} else {
+			pc++
+		}
+	}
+	return bits
+}
+
 //====================================================================================
 // Utils func
 //====================================================================================
