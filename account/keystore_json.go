e9b75a8c5055d51118b80a2e2040be1dd9e12fec
Kien Dang <kien.15@gmail.com>
2018-08-07 03:28:17 +0700

    remove unused function

--- account/keystore_json.go
+++ account/keystore_json.go
@@ -113,24 +113,3 @@ func GetPrivateKey(derivedKey, cipherText, iv []byte) (*ecdsa.PrivateKey, error)
 
 	return crypto.ToECDSAUnsafe(privateKey), nil
 }
-
-// From https://leanpub.com/gocrypto/read#leanpub-auto-block-cipher-modes
-func pkcs7Unpad(in []byte) []byte {
-	if len(in) == 0 {
-		return nil
-	}
-
-	padding := in[len(in)-1]
-	if int(padding) > len(in) || padding > aes.BlockSize {
-		return nil
-	} else if padding == 0 {
-		return nil
-	}
-
-	for i := len(in) - 1; i > len(in)-int(padding)-1; i-- {
-		if in[i] != padding {
-			return nil
-		}
-	}
-	return in[:len(in)-int(padding)]
-}
