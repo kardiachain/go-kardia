3451757056ef4c87f08e72d39dc4cadc110b1acb
Kien Dang <kien.15@gmail.com>
2018-08-26 18:06:00 +0700

    Add API to get list of peers

--- node/api.go
+++ node/api.go
@@ -10,9 +10,20 @@ func NewPublicNodeAPI(node *Node) *PublicNodeAPI {
 	return &PublicNodeAPI{node}
 }
 
-// PeersList returns the number of peers that current node can connect to.
+// PeersCount returns the number of peers that current node can connect to.
 func (s *PublicNodeAPI) PeersCount() int {
 	return s.node.server.PeerCount()
+}
+
+
+// Peers returns a list of peers
+func (s *PublicNodeAPI) Peers() map[string]string{
+	peers := s.node.server.Peers()
+	results := make(map[string]string)
+	for _, peer := range peers {
+		results[peer.Name()] = peer.RemoteAddr().String()
+	}
+	return results
 }
 
 
