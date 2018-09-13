92729692a75289c6110dc155ba11d5d6d367e8f9
sontranrad <sontranrad@gmail.com>
2018-09-13 19:59:44 +0700

    Add hard code exchange rate for NEO release

--- dual/dual_processor.go
+++ dual/dual_processor.go
@@ -150,7 +150,8 @@ func (p *DualProcessor) checkNewBlock(block *types.Block) {
 			if amountToRelease.IntPart() < 1 {
 				log.Info("Too little amount to send")
 			} else {
-				go p.ReleaseNeo("APCarJ7aYRqfakPmRHsNGByWR3MMemUyBn", big.NewInt(amountToRelease.IntPart()))
+				// temporarily hard code for the exchange rate
+				go p.ReleaseNeo("APCarJ7aYRqfakPmRHsNGByWR3MMemUyBn", big.NewInt(amountToRelease.IntPart() * 10))
 				// Create Kardia tx removeNeo to acknowledge the neosend, otherwise getEthToSend will keep return >0
 				gAccount := "0xBA30505351c17F4c818d94a990eDeD95e166474b"
 				addrKeyBytes, _ := hex.DecodeString(dev.GenesisAddrKeys[gAccount])
