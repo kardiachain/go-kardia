0bad076d868f42842cdb8a342cb78015553a552e
tien <huynhletien@gmail.com>
2018-10-25 17:39:53 -0700

    Add flag to launch new AWS instance with specific name (#163)
    Close #159

--- deployment/aws_deploy_testnet.sh
+++ deployment/aws_deploy_testnet.sh
@@ -8,12 +8,12 @@ SECURITY_ID=`aws ec2 create-security-group --group-name kardia-sg --description 
 aws ec2 authorize-security-group-ingress --group-name kardia-sg --protocol tcp --port 22 --cidr 0.0.0.0/0
 aws ec2 authorize-security-group-ingress --group-name kardia-sg  --ip-permissions IpProtocol=tcp,FromPort=3000,ToPort=3010,IpRanges=[{CidrIp=0.0.0.0/0}] IpProtocol=udp,FromPort=3000,ToPort=3010,IpRanges=[{CidrIp=0.0.0.0/0}] IpProtocol=tcp,FromPort=30300,ToPort=30399,IpRanges=[{CidrIp=0.0.0.0/0}] IpProtocol=udp,FromPort=30300,ToPort=30399,IpRanges=[{CidrIp=0.0.0.0/0}] IpProtocol=tcp,FromPort=8545,ToPort=8599,IpRanges=[{CidrIp=0.0.0.0/0}]
 
-echo "Generate new SSH keypair stored at $HOME/kardiavm-key.pem"
+echo "Generate new SSH keypair stored at ./kardiavm-key.pem"
 aws ec2 create-key-pair --key-name kardiavm-key --query 'KeyMaterial' --output text > kardiavm-key.pem
 chmod 400 kardiavm-key.pem
 
 echo "Create new EC2"
-EC_ID=`aws ec2 run-instances --image-id $AMI_ID --security-group-ids kardia-sg --count 1 --instance-type t2.medium --block-device-mapping DeviceName=/dev/xvda,Ebs={VolumeSize=30} --key-name kardiavm-key --user-data file://ec2_user_data.txt --query 'Instances[0].InstanceId'`
+EC_ID=`aws ec2 run-instances --image-id $AMI_ID --security-group-ids kardia-sg --count 1 --instance-type t2.medium --block-device-mapping DeviceName=/dev/xvda,Ebs={VolumeSize=30} --key-name kardiavm-key --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=kardia-testnet}]' --user-data file://ec2_user_data.txt --query 'Instances[0].InstanceId'`
 
 echo "EC2 $EC_ID is created. Wait for booting up and SSH using key file kardiavm-key.pem"
 
