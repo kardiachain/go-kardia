1b99b81a120ba38d66ee9b26440c0395ce1553d0
namdoh <namdoh@gmail.com>
2019-06-24 01:20:09 +0000

    Add PartSet to start breaking block size to small chuncks for consensus.

---
+++ types/validation.go
@@ -0,0 +1,19 @@
+package types
+
+import (
+	"fmt"
+
+	"github.com/kardiachain/go-kardia/lib/merkle"
+)
+
+// ValidateHash returns an error if the hash is not empty, but its
+// size != merkle.TmHashSize.
+func ValidateHash(h []byte) error {
+	if len(h) > 0 && len(h) != merkle.TmHashSize {
+		return fmt.Errorf("Expected size to be %d bytes, got %d bytes",
+			merkle.TmHashSize,
+			len(h),
+		)
+	}
+	return nil
+}
