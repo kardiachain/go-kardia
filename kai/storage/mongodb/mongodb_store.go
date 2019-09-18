5aaaf1616463cf4deb9e3abb6e1ab5c37d97671a
kien Dang <kien.15@gmail.com>
2019-09-18 15:18:34 +0700

    Add timeout for context

--- kai/storage/mongodb/mongodb_store.go
+++ kai/storage/mongodb/mongodb_store.go
@@ -29,6 +29,7 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/x/bsonx"
+	"time"
 )
 
 type Store struct {
@@ -37,8 +38,10 @@ type Store struct {
 }
 
 func NewClient(uri string) (*mongo.Client, *context.Context, error) {
-	ctx := context.Background()
-	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(uri))
+	// add timeout for context
+	// TODO: move timeout to config
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
 	if err != nil {
 		return nil, nil, err
 	}
