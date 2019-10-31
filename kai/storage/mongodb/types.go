580788049a00eb46127f6d4bcd2d898f8ad6a67d
thangn <thangn.1411@gmail.com>
2019-10-31 08:10:25 +0700

    update mongodb convert block id

--- kai/storage/mongodb/types.go
+++ kai/storage/mongodb/types.go
@@ -46,6 +46,11 @@ const (
 )
 
 type (
+	PartSetHeader struct {
+		Total int
+		Hash  string
+	}
+
 	Header struct {
 		Height        uint64 `json:"height"        bson:"height"`
 		Time          uint64 `json:"time"          bson:"time"`
@@ -56,8 +61,9 @@ type (
 		GasUsed  uint64 `json:"gasUsed"          bson:"gasUsed"`
 
 		// prev block info
-		LastBlockID string `json:"lastBlockID"      bson:"lastBlockID"`
-		Coinbase    string `json:"miner"            bson:"miner"` // address
+		LastBlockID string        `json:"lastBlockID"      bson:"lastBlockID"`
+		PartsHeader PartSetHeader `json:"partsHeader" 		  bson:"partsHeader"`
+		Coinbase    string        `json:"miner"            bson:"miner"` // address
 
 		// hashes of block data
 		LastCommitHash string `json:"lastCommitHash"      bson:"lastCommitHash"` // commit from validators from the last block
@@ -149,20 +155,22 @@ type (
 	}
 
 	Commit struct {
-		Height     uint64  `json:"height"           bson:"height"`
-		BlockID    string  `json:"blockID"          bson:"blockID"`
-		Precommits []*Vote `json:"precommits"       bson:"precommits"`
+		Height      uint64        `json:"height"           bson:"height"`
+		BlockID     string        `json:"blockID"          bson:"blockID"`
+		Precommits  []*Vote       `json:"precommits"       bson:"precommits"`
+		PartsHeader PartSetHeader `json:"partsHeader" bson:"partsHeader"`
 	}
 
 	Vote struct {
-		ValidatorAddress string `json:"validatorAddress"           bson:"validatorAddress"`
-		ValidatorIndex   int64  `json:"validatorIndex"             bson:"validatorIndex"`
-		Height           int64  `json:"height"                     bson:"height"`
-		Round            int64  `json:"round"                      bson:"round"`
-		Timestamp        uint64 `json:"timestamp"                  bson:"timestamp"`
-		Type             byte   `json:"type"                       bson:"type"`
-		BlockID          string `json:"blockID"                    bson:"blockID"`
-		Signature        string `json:"signature"                  bson:"signature"`
+		ValidatorAddress string        `json:"validatorAddress"           bson:"validatorAddress"`
+		ValidatorIndex   int64         `json:"validatorIndex"             bson:"validatorIndex"`
+		Height           int64         `json:"height"                     bson:"height"`
+		Round            int64         `json:"round"                      bson:"round"`
+		Timestamp        uint64        `json:"timestamp"                  bson:"timestamp"`
+		Type             byte          `json:"type"                       bson:"type"`
+		BlockID          string        `json:"blockID"                    bson:"blockID"`
+		Signature        string        `json:"signature"                  bson:"signature"`
+		PartsHeader      PartSetHeader `json:"partsHeader" bson:"partsHeader"`
 	}
 
 	HeadHeaderHash struct {
@@ -241,7 +249,7 @@ func NewBlock(block *types.Block) *Block {
 func (block *Block) ToHeader() *types.Header {
 	header := types.Header{
 		Height:         block.Header.Height,
-		LastBlockID:    (types.BlockID)(common.HexToHash(block.Header.LastBlockID)),
+		LastBlockID:    toBlockID(block.Header.LastBlockID, block.Header.PartsHeader),
 		Time:           big.NewInt(int64(block.Header.Time)),
 		NumTxs:         block.Header.NumTxs,
 		TxHash:         common.HexToHash(block.Header.TxHash),
@@ -457,7 +465,7 @@ func (vote *Vote) ToVote() *types.Vote {
 		Type:             vote.Type,
 		Timestamp:        big.NewInt(int64(vote.Timestamp)),
 		Round:            common.NewBigInt64(vote.Round),
-		BlockID:          (types.BlockID)(common.HexToHash(vote.BlockID)),
+		BlockID:          toBlockID(vote.BlockID, vote.PartsHeader),
 		Signature:        common.Hex2Bytes(vote.Signature),
 		ValidatorAddress: common.HexToAddress(vote.ValidatorAddress),
 		ValidatorIndex:   common.NewBigInt64(vote.ValidatorIndex),
@@ -491,7 +499,7 @@ func (commit *Commit) ToCommit() *types.Commit {
 	}
 	return &types.Commit{
 		Precommits: votes,
-		BlockID:    (types.BlockID)(common.HexToHash(commit.BlockID)),
+		BlockID:    toBlockID(commit.BlockID, commit.PartsHeader),
 	}
 }
 
@@ -517,4 +525,14 @@ func (config *ChainConfig) ToChainConfig() *types.ChainConfig {
 		Period: config.Period,
 	}
 	return &types.ChainConfig{Kaicon: &kaiCon, BaseAccount: &types.BaseAccount{PrivateKey: *pk, Address: common.HexToAddress(config.BaseAccount.Address)}}
+}
+
+func toBlockID(blockID string, partSetHeader PartSetHeader) types.BlockID {
+	return types.BlockID{
+		Hash: common.BytesToHash([]byte(blockID)),
+		PartsHeader: types.PartSetHeader{
+			Total: *common.NewBigInt32(partSetHeader.Total),
+			Hash:  common.BytesToHash([]byte(partSetHeader.Hash)),
+		},
+	}
 }
