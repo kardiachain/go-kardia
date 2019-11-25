63f9ca08fe0801b8c3276bc658a6d6b8536b1928
thangn <thangn.1411@gmail.com>
2019-11-25 16:59:42 +0700

    update copyright

--- mainchain/tx_pool/tx_pool_helper.go
+++ mainchain/tx_pool/tx_pool_helper.go
@@ -15,7 +15,6 @@
  *  You should have received a copy of the GNU Lesser General Public License
  *  along with the go-kardia library. If not, see <http://www.gnu.org/licenses/>.
  */
-
 package tx_pool
 
 import (
