d1294c41357953dda136e2a54f5372b41239d3c0
lewtran <lewtran@outlook.com>
2020-08-27 16:09:17 +0700

    fix typo

--- lib/common/compress_fuzz.go
+++ lib/common/compress_fuzz.go
@@ -15,9 +15,6 @@
  *  You should have received a copy of the GNU Lesser General Public License
  *  along with the go-kardia library. If not, see <http://www.gnu.org/licenses/>.
  */
-
-// +build gofuzz
-
 package common
 
 import "bytes"
