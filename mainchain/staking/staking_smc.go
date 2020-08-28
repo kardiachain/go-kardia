9a57fed825103424cf366a0e4d1448f503065a2a
duypv1997 <phamvanduy2811997@gmail.com>
2020-08-28 16:15:51 +0700

    add function and test staking

--- mainchain/staking/staking_smc.go
+++ mainchain/staking/staking_smc.go
@@ -37,8 +37,7 @@ func NewSmcStakingnUtil(bc base.BaseBlockChain) (*StakingSmcUtil, error) {
 	}
 
 	stakingSmcAddr, stakingSmcAbi := configs.GetContractDetailsByIndex(KardiaSatkingSmcIndex)
-	// fmt.Printf("sasasasasasa %s", stakingSmcAddr)
-	// fmt.Printf("stakingSmcAbi %s", stakingSmcAbi)
+
 	bytecodeStaking := configs.GetContractByteCodeByAddress(contractAddress)
 	if stakingSmcAbi == "" {
 		log.Error("Error getting abi by index")
@@ -74,7 +73,7 @@ func (s *StakingSmcUtil) SetInflation(number int64, SenderAddress common.Address
 }
 
 func (s *StakingSmcUtil) GetInflation(SenderAddress common.Address) (*big.Int, error) {
-	stateDb := s.StateDb                                             // nen viet 1 lan
+	stateDb := s.StateDb
 	stateDb.SetCode(s.ContractAddress, common.Hex2Bytes(s.bytecode)) //set contract to stateDB
 
 	get, err := s.Abi.Pack("getInflation")
@@ -152,7 +151,6 @@ func (s *StakingSmcUtil) SetParams(baseProposerReward int64, bonusProposerReward
 
 	_, _, err = sample_kvm.Call(s.ContractAddress, store, &sample_kvm.Config{State: stateDb, Origin: SenderAddress})
 	if err != nil {
-		fmt.Printf("err %s", err)
 		return nil, err
 	}
 
@@ -160,23 +158,341 @@ func (s *StakingSmcUtil) SetParams(baseProposerReward int64, bonusProposerReward
 }
 
 //create validator
-func (s *StakingSmcUtil) CreateValidator(maxRate int64, maxChangeRate int64, minSelfDelegation int64, SenderAddress common.Address) (*big.Int, error) {
-
+func (s *StakingSmcUtil) CreateValidator(commssionRate int64, maxRate int64, maxChangeRate int64, minSelfDelegation int64, SenderAddress common.Address, amount int64) (*big.Int, error) {
 	stateDb := s.StateDb
 	stateDb.SetCode(s.ContractAddress, common.Hex2Bytes(s.bytecode))
 
-	// validator1 := common.HexToAddress("0xc1fe56E3F58D3244F606306611a5d10c8333f1f6")
-	// maxRate := big.NewInt(20)
-	// maxChangeRate := big.NewInt(5)
-	// minSelfDelegation := big.NewInt(5)
-	createValidator, err := s.Abi.Pack("createValidator", big.NewInt(2), big.NewInt(maxRate), big.NewInt(maxChangeRate), big.NewInt(minSelfDelegation))
+	createValidator, err := s.Abi.Pack("createValidator", big.NewInt(commssionRate), big.NewInt(maxRate), big.NewInt(maxChangeRate), big.NewInt(minSelfDelegation))
 	if err != nil {
 		return nil, err
 	}
-	_, _, err = sample_kvm.Call(s.ContractAddress, createValidator, &sample_kvm.Config{State: stateDb, Value: big.NewInt(30), Origin: SenderAddress})
+	_, _, err = sample_kvm.Call(s.ContractAddress, createValidator, &sample_kvm.Config{State: stateDb, Value: big.NewInt(amount), Origin: SenderAddress})
 	if err != nil {
 		return nil, err
 	}
 
 	return nil, nil
+}
+
+//applyAndReturnValidatorSets allow appy and return validator set
+func (s *StakingSmcUtil) ApplyAndReturnValidatorSets(SenderAddress common.Address) error {
+	stateDb := s.StateDb
+	stateDb.SetCode(s.ContractAddress, common.Hex2Bytes(s.bytecode))
+
+	applyAndReturnValidatorSets, err := s.Abi.Pack("applyAndReturnValidatorSets")
+	if err != nil {
+		return err
+	}
+	_, _, err = sample_kvm.Call(s.ContractAddress, applyAndReturnValidatorSets, &sample_kvm.Config{State: stateDb, Origin: SenderAddress})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+//getValidatorSets allow get validator set
+func (s *StakingSmcUtil) GetValidatorSets() ([]common.Address, []*big.Int, error) {
+	stateDb := s.StateDb
+	stateDb.SetCode(s.ContractAddress, common.Hex2Bytes(s.bytecode))
+
+	getValidatorSets, err := s.Abi.Pack("getValidatorSets")
+	if err != nil {
+		return nil, nil, err
+	}
+	resultGet, _, err := sample_kvm.Call(s.ContractAddress, getValidatorSets, &sample_kvm.Config{State: stateDb})
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var validatorSet struct {
+		ValAddrs []common.Address
+		Powers   []*big.Int
+	}
+
+	//unpack result
+	err = s.Abi.Unpack(&validatorSet, "getValidatorSets", resultGet)
+	if err != nil {
+		log.Error("Error unpacking node info", "err", err)
+	}
+
+	return validatorSet.ValAddrs, validatorSet.Powers, nil
+}
+
+//getValidatorSets allow get validator
+func (s *StakingSmcUtil) GetValidator(valAddress common.Address) (*big.Int, *big.Int, bool, error) {
+	stateDb := s.StateDb
+	stateDb.SetCode(s.ContractAddress, common.Hex2Bytes(s.bytecode))
+
+	getValidator, err := s.Abi.Pack("getValidator", valAddress)
+	if err != nil {
+		return nil, nil, false, err
+	}
+	resultGet, _, err := sample_kvm.Call(s.ContractAddress, getValidator, &sample_kvm.Config{State: stateDb})
+	if err != nil {
+		return nil, nil, false, err
+	}
+
+	var validatorInfor struct {
+		Tokens           *big.Int
+		DelegationShares *big.Int
+		Jailed           bool
+	}
+
+	//unpack result
+	err = s.Abi.Unpack(&validatorInfor, "getValidator", resultGet)
+	if err != nil {
+		log.Error("Error unpacking node info", "err", err)
+	}
+
+	return validatorInfor.Tokens, validatorInfor.DelegationShares, validatorInfor.Jailed, nil
+}
+
+//mints new tokens for the previous block. Returns fee collected
+func (s *StakingSmcUtil) Mint() (*big.Int, error) {
+	stateDb := s.StateDb
+	stateDb.SetCode(s.ContractAddress, common.Hex2Bytes(s.bytecode))
+
+	mint, err := s.Abi.Pack("mint")
+	if err != nil {
+		return nil, err
+	}
+	resultGet, _, err := sample_kvm.Call(s.ContractAddress, mint, &sample_kvm.Config{State: stateDb})
+	if err != nil {
+		return nil, err
+	}
+
+	num := new(big.Int).SetBytes(resultGet)
+	if err != nil {
+		return nil, err
+	}
+
+	return num, nil
+}
+
+//mints new tokens for the previous block. Returns fee collected
+func (s *StakingSmcUtil) SetTotalBonded(totalBond int64, SenderAddress common.Address) error {
+	stateDb := s.StateDb
+	stateDb.SetCode(s.ContractAddress, common.Hex2Bytes(s.bytecode))
+
+	setTotalBonded, err := s.Abi.Pack("setTotalBonded", big.NewInt(totalBond))
+	if err != nil {
+		return err
+	}
+	_, _, err = sample_kvm.Call(s.ContractAddress, setTotalBonded, &sample_kvm.Config{State: stateDb, Origin: SenderAddress})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+//SetAnnualProvision allow et annual provisoin
+func (s *StakingSmcUtil) SetAnnualProvision(annualProvision int64, SenderAddress common.Address) error {
+	stateDb := s.StateDb
+	stateDb.SetCode(s.ContractAddress, common.Hex2Bytes(s.bytecode))
+
+	setAnnualProvision, err := s.Abi.Pack("setAnnualProvision", big.NewInt(annualProvision))
+	if err != nil {
+		return err
+	}
+	_, _, err = sample_kvm.Call(s.ContractAddress, setAnnualProvision, &sample_kvm.Config{State: stateDb, Origin: SenderAddress})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+//GetAnnualProvision allow get annual provision
+func (s *StakingSmcUtil) GetBlockProvision() (*big.Int, error) {
+	stateDb := s.StateDb
+	stateDb.SetCode(s.ContractAddress, common.Hex2Bytes(s.bytecode))
+
+	getBlockProvision, err := s.Abi.Pack("getBlockProvision")
+	if err != nil {
+		return nil, err
+	}
+	resultGet, _, err := sample_kvm.Call(s.ContractAddress, getBlockProvision, &sample_kvm.Config{State: stateDb})
+	if err != nil {
+		return nil, err
+	}
+
+	num := new(big.Int).SetBytes(resultGet)
+	if err != nil {
+		return nil, err
+	}
+
+	return num, nil
+}
+
+//mints new tokens for the previous block. Returns fee collected
+func (s *StakingSmcUtil) SetMintParams(inflationRateChange int64, goalBonded int64, blocksPerYear int64,
+	inflationMax int64, inflationMin int64, SenderAddress common.Address) error {
+
+	stateDb := s.StateDb
+	stateDb.SetCode(s.ContractAddress, common.Hex2Bytes(s.bytecode))
+
+	setMintParams, err := s.Abi.Pack("setMintParams", big.NewInt(inflationRateChange), big.NewInt(goalBonded), big.NewInt(blocksPerYear),
+		big.NewInt(inflationMax), big.NewInt(inflationMin))
+	if err != nil {
+		return err
+	}
+	_, _, err = sample_kvm.Call(s.ContractAddress, setMintParams, &sample_kvm.Config{State: stateDb, Origin: SenderAddress})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+//setPreviousProposer allow set previous proposer
+func (s *StakingSmcUtil) SetPreviousProposer(previousProposer common.Address, SenderAddress common.Address) error {
+	stateDb := s.StateDb
+	stateDb.SetCode(s.ContractAddress, common.Hex2Bytes(s.bytecode))
+
+	setPreviousProposer, err := s.Abi.Pack("setPreviousProposer", previousProposer)
+	if err != nil {
+		return err
+	}
+	_, _, err = sample_kvm.Call(s.ContractAddress, setPreviousProposer, &sample_kvm.Config{State: stateDb, Origin: SenderAddress})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+//FinalizeCommit
+func (s *StakingSmcUtil) FinalizeCommit(address []common.Address, powers []*big.Int, signed []bool, SenderAddress common.Address) error {
+	stateDb := s.StateDb
+	stateDb.SetCode(s.ContractAddress, common.Hex2Bytes(s.bytecode))
+
+	finalizeCommit, err := s.Abi.Pack("finalizeCommit", address, powers, signed)
+	if err != nil {
+		return err
+	}
+	_, _, err = sample_kvm.Call(s.ContractAddress, finalizeCommit, &sample_kvm.Config{State: stateDb, Origin: SenderAddress})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+//getMissedBlock allow get missed block of validator
+func (s *StakingSmcUtil) GetMissedBlock(valAddress common.Address) ([]bool, error) {
+	stateDb := s.StateDb
+	stateDb.SetCode(s.ContractAddress, common.Hex2Bytes(s.bytecode))
+
+	getMissedBlock, err := s.Abi.Pack("getMissedBlock", valAddress)
+	if err != nil {
+		return nil, err
+	}
+	resultGet, _, err := sample_kvm.Call(s.ContractAddress, getMissedBlock, &sample_kvm.Config{State: stateDb})
+	if err != nil {
+		return nil, err
+	}
+
+	var missed struct {
+		MissedBlock []bool
+	}
+
+	//unpack result
+	err = s.Abi.Unpack(&missed, "getMissedBlock", resultGet)
+	if err != nil {
+		log.Error("Error unpacking", "err", err)
+	}
+
+	return missed.MissedBlock, nil
+}
+
+//GetAnnualProvision allow get delegation rewards
+func (s *StakingSmcUtil) GetDelegationRewards(valAddress common.Address, delAddress common.Address) (*big.Int, error) {
+	stateDb := s.StateDb
+	stateDb.SetCode(s.ContractAddress, common.Hex2Bytes(s.bytecode))
+
+	getDelegationRewards, err := s.Abi.Pack("getDelegationRewards", valAddress, delAddress)
+	if err != nil {
+		return nil, err
+	}
+	resultGet, _, err := sample_kvm.Call(s.ContractAddress, getDelegationRewards, &sample_kvm.Config{State: stateDb})
+	if err != nil {
+		return nil, err
+	}
+
+	num := new(big.Int).SetBytes(resultGet)
+	if err != nil {
+		return nil, err
+	}
+
+	return num, nil
+}
+
+//double sign
+func (s *StakingSmcUtil) DoubleSign(valAddress common.Address, votingPower int64, distributionHeight int64, SenderAddress common.Address) error {
+	stateDb := s.StateDb
+	stateDb.SetCode(s.ContractAddress, common.Hex2Bytes(s.bytecode))
+
+	doubleSign, err := s.Abi.Pack("doubleSign", valAddress, big.NewInt(votingPower), big.NewInt(distributionHeight))
+	if err != nil {
+		return err
+	}
+	_, _, err = sample_kvm.Call(s.ContractAddress, doubleSign, &sample_kvm.Config{State: stateDb, Origin: SenderAddress})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+//withdraw
+func (s *StakingSmcUtil) Withdraw(valAddress common.Address, SenderAddress common.Address) error {
+	stateDb := s.StateDb
+	stateDb.SetCode(s.ContractAddress, common.Hex2Bytes(s.bytecode))
+
+	doubleSign, err := s.Abi.Pack("withdraw", valAddress)
+	if err != nil {
+		return err
+	}
+	_, _, err = sample_kvm.Call(s.ContractAddress, doubleSign, &sample_kvm.Config{State: stateDb, Origin: SenderAddress})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+//undelegate
+func (s *StakingSmcUtil) Undelegate(valAddress common.Address, amount int64, SenderAddress common.Address) error {
+	stateDb := s.StateDb
+	stateDb.SetCode(s.ContractAddress, common.Hex2Bytes(s.bytecode))
+
+	undelegate, err := s.Abi.Pack("undelegate", valAddress, big.NewInt(amount))
+	if err != nil {
+		return err
+	}
+	_, _, err = sample_kvm.Call(s.ContractAddress, undelegate, &sample_kvm.Config{State: stateDb, Origin: SenderAddress})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+//unjail
+func (s *StakingSmcUtil) Unjail(SenderAddress common.Address) error {
+	stateDb := s.StateDb
+	stateDb.SetCode(s.ContractAddress, common.Hex2Bytes(s.bytecode))
+
+	unjail, err := s.Abi.Pack("unjail")
+	if err != nil {
+		return err
+	}
+	_, _, err = sample_kvm.Call(s.ContractAddress, unjail, &sample_kvm.Config{State: stateDb, Origin: SenderAddress})
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- mainchain/staking/staking_smc_test.go
+++ mainchain/staking/staking_smc_test.go
@@ -75,18 +75,6 @@ func GetSmcStakingUtil() (*StakingSmcUtil, error) {
 	return util, nil
 }
 
-func TestGetSmcStakingUtil(t *testing.T) {
-	bc, err := GetBlockchain()
-	if err != nil {
-		t.Log(err)
-	}
-	util, err := NewSmcStakingnUtil(bc)
-	if err != nil {
-		t.Log(err)
-	}
-	t.Log(util)
-}
-
 func TestInflation(t *testing.T) {
 	util, err := GetSmcStakingUtil()
 	if err != nil {
@@ -95,12 +83,9 @@ func TestInflation(t *testing.T) {
 
 	address := common.HexToAddress("0x7cefC13B6E2aedEeDFB7Cb6c32457240746BAEe5")
 	// Check valid node with correct type
-	store, err := util.SetInflation(1000, address)
+	_, err = util.SetInflation(1000, address)
 	if err != nil {
 		t.Fatal(err)
-	}
-	if store != nil {
-		t.Log("storeeeee", store)
 	}
 
 	get, err := util.GetInflation(address)
@@ -123,12 +108,9 @@ func TestTotalSupply(t *testing.T) {
 
 	address := common.HexToAddress("0x7cefC13B6E2aedEeDFB7Cb6c32457240746BAEe5")
 	// Check valid node with correct type
-	store, err := util.SetTotalSupply(9000, address)
+	_, err = util.SetTotalSupply(9000, address)
 	if err != nil {
 		t.Fatal(err)
-	}
-	if store != nil {
-		t.Log("storeeeee", store)
 	}
 
 	get, err := util.GetTotalSupply(address)
@@ -155,16 +137,12 @@ func TestSetParams(t *testing.T) {
 	// unBondingTime := 1
 	// signedBlockWindow := 2
 	// minSignedBlockPerWindow := 1
-	store, err := util.SetParams(1000000000000, 100000000000, 1000000000000, 5000000000000,
+	_, err = util.SetParams(1000000000000, 100000000000, 1000000000000, 5000000000000,
 		1, 2, 5000000000000, address)
 
 	if err != nil {
 		t.Fatal(err)
 	}
-	if store != nil {
-		t.Log("storeeeee", store)
-	}
-
 }
 
 func TestCreateValidator(t *testing.T) {
@@ -175,7 +153,41 @@ func TestCreateValidator(t *testing.T) {
 
 	address := common.HexToAddress("0xc1fe56E3F58D3244F606306611a5d10c8333f1f6")
 
-	store, err := util.CreateValidator(20, 5, 5, address)
+	_, err = util.CreateValidator(2, 20, 5, 5, address, 3999999999)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	//get validator
+	token, delShare, jail, err := util.GetValidator(address)
+	if err != nil {
+		t.Log("Error", err)
+	}
+
+	if token.Cmp(big.NewInt(3999999999)) != 0 {
+		t.Error("Expected 3999999999, got ", token)
+	}
+
+	if delShare.Cmp(big.NewInt(1000000000000000000)) != 0 {
+		t.Error("Expected 1000000000000000000, got ", delShare)
+	}
+
+	if jail {
+		t.Error("Expected false, got ", jail)
+	}
+
+}
+
+func TestSetAndGetValidatorSet(t *testing.T) {
+	util, err := GetSmcStakingUtil()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	address := common.HexToAddress("0xc1fe56E3F58D3244F606306611a5d10c8333f1f6")
+
+	store, err := util.CreateValidator(2, 20, 5, 5, address, 30)
 
 	if err != nil {
 		t.Fatal(err)
@@ -184,4 +196,226 @@ func TestCreateValidator(t *testing.T) {
 		t.Log("storeeeee", store)
 	}
 
+	err = util.ApplyAndReturnValidatorSets(address)
+	if err != nil {
+		t.Log(err)
+	}
+	_, _, err = util.GetValidatorSets()
+	if err != nil {
+		t.Log(err)
+	}
+}
+
+func TestMint(t *testing.T) {
+	util, err := GetSmcStakingUtil()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	address := common.HexToAddress("0x7cefC13B6E2aedEeDFB7Cb6c32457240746BAEe5")
+	err = util.SetMintParams(1, 1, 5, 1, 1, address)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = util.SetTotalSupply(10000, address)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = util.SetTotalBonded(1000, address)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = util.SetInflation(0, address)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = util.SetAnnualProvision(1, address)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = util.Mint()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	inflation, err := util.GetInflation(address)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if inflation.Cmp(big.NewInt(1)) != 0 {
+		t.Error("Expected 1, got ", inflation)
+	}
+}
+
+func TestFinalizeCommit(t *testing.T) {
+	util, err := GetSmcStakingUtil()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	valAddress1 := common.HexToAddress("0xc1fe56E3F58D3244F606306611a5d10c8333f1f6")
+
+	_, err = util.SetParams(1000000000000, 100000000000, 1000000000000, 5000000000000,
+		1, 3, 5000000000000, valAddress1)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = util.CreateValidator(2, 20, 5, 5, valAddress1, 30)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	addrs := []common.Address{valAddress1}
+	powers := []*big.Int{big.NewInt(1)}
+	signed := []bool{false}
+
+	err = util.FinalizeCommit(addrs, powers, signed, valAddress1)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	missedBlock, err := util.GetMissedBlock(valAddress1)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !missedBlock[0] {
+		t.Error("Expected true, got ", missedBlock)
+	}
+
+}
+
+func TestDoubleSign(t *testing.T) {
+	util, err := GetSmcStakingUtil()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	valAddress1 := common.HexToAddress("0xc1fe56E3F58D3244F606306611a5d10c8333f1f6")
+
+	_, err = util.SetParams(1000000000000, 100000000000, 1000000000000, 5000000000000,
+		1, 3, 5000000000000, valAddress1)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = util.CreateValidator(2, 20, 5, 5, valAddress1, 30)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	//get validator
+	token, _, _, err := util.GetValidator(valAddress1)
+	if err != nil {
+		t.Log("Error", err)
+	}
+
+	if token.Cmp(big.NewInt(30)) != 0 {
+		t.Error("Expected 30, got ", token)
+	}
+
+	err = util.DoubleSign(valAddress1, 1000000, 10, valAddress1)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	//slash 100% token
+	token, _, _, err = util.GetValidator(valAddress1)
+	if err != nil {
+		t.Log("Error", err)
+	}
+
+	if token.Cmp(big.NewInt(0)) != 0 {
+		t.Error("Expected 0, got ", token)
+	}
+}
+
+func TestUndelegate(t *testing.T) {
+	util, err := GetSmcStakingUtil()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	valAddress1 := common.HexToAddress("0xc1fe56E3F58D3244F606306611a5d10c8333f1f6")
+
+	_, err = util.SetParams(1000000000000, 100000000000, 1000000000000, 5000000000000,
+		1, 3, 5000000000000, valAddress1)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = util.CreateValidator(2, 20, 5, 5, valAddress1, 30)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = util.SetTotalSupply(1000, valAddress1)
+
+	addrs := []common.Address{}
+	powers := []*big.Int{}
+	signed := []bool{}
+
+	err = util.FinalizeCommit(addrs, powers, signed, valAddress1)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = util.Undelegate(valAddress1, 30, valAddress1)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	token, _, _, err := util.GetValidator(valAddress1)
+	if err != nil {
+		t.Log("Error", err)
+	}
+	t.Log(token)
+}
+
+//should jail when self delegation too low
+func TestJail(t *testing.T) {
+	util, err := GetSmcStakingUtil()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	valAddress1 := common.HexToAddress("0xc1fe56E3F58D3244F606306611a5d10c8333f1f6")
+
+	_, err = util.SetParams(1000000000000, 100000000000, 1000000000000, 5000000000000,
+		1, 3, 5000000000000, valAddress1)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = util.CreateValidator(2, 20, 5, 5, valAddress1, 30)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = util.SetTotalSupply(1000, valAddress1)
+
+	err = util.Undelegate(valAddress1, 29, valAddress1)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	token, _, jail, err := util.GetValidator(valAddress1)
+	if err != nil {
+		t.Log("Error", err)
+	}
+
+	if token.Cmp(big.NewInt(2)) != 0 {
+		t.Error("Expected 0, got ", token)
+	}
+
+	if !jail {
+		t.Error("Expected true, got ", false)
+	}
 }
