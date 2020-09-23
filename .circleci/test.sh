709e91ecd475e1e1a8e4a91a80fcfc1f3210de5f
lewtran <lewtran@outlook.com>
2020-09-23 16:12:45 +0700

    Disable timeout in ci-test

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -4,7 +4,7 @@ set -e
 echo "" > coverage.txt
 
 for d in $(go list ./... | grep -v vendor); do
-    go test -coverprofile=profile.out -covermode=atomic $d
+    go test -coverprofile=profile.out -covermode=atomic $d -test.timeout=0
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
         rm profile.out
