41705db76d9293043b8a60a599417b25b21dd067
thangn <thangn.1411@gmail.com>
2020-11-03 21:38:20 +0700

    implement gas used

--- kai/base/blockchain.go
+++ kai/base/blockchain.go
@@ -38,7 +38,7 @@ type BaseBlockChain interface {
 	GetBlockByHash(hash common.Hash) *types.Block
 	State() (*state.StateDB, error)
 	CommitTrie(root common.Hash) error
-	WriteReceipts(receipts types.Receipts, block *types.Block)
+	WriteBlockInfo(blockInfo *types.BlockInfo, block *types.Block)
 	ReadCommit(height uint64) *types.Commit
 	Config() *configs.ChainConfig
 	GetHeader(common.Hash, uint64) *types.Header
