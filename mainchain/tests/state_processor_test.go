f488400908a9cd16b76e93632fe9ebcc2e6137e7
lewtran <lewtran@outlook.com>
2020-11-10 15:29:01 +0700

    Fix RPC unit test

--- mainchain/tests/state_processor_test.go
+++ mainchain/tests/state_processor_test.go
@@ -116,14 +116,14 @@ func execute(bc *blockchain.BlockChain, msg types.Message) ([]byte, error) {
 		IsZeroFee: true,
 	})
 
-	ret, usedGas, failed, err := blockchain.NewStateTransition(vmenv, msg, gasPool).TransitionDb()
+	res, err := blockchain.NewStateTransition(vmenv, msg, gasPool).TransitionDb()
 	if err != nil {
 		return nil, fmt.Errorf("%v", err)
 	}
-	if failed {
+	if res.Failed() {
 		return nil, errors.New("transaction failed")
 	}
-	if usedGas != 0 {
+	if res.UsedGas != 0 {
 		return nil, errors.New("usedGas must be zero")
 	}
 
@@ -132,7 +132,7 @@ func execute(bc *blockchain.BlockChain, msg types.Message) ([]byte, error) {
 		return nil, errors.New("originBalance should equal to balance")
 	}
 
-	return ret, nil
+	return res.ReturnData, nil
 }
 
 func executeWithFee(bc *blockchain.BlockChain, msg types.Message) ([]byte, error) {
@@ -153,14 +153,14 @@ func executeWithFee(bc *blockchain.BlockChain, msg types.Message) ([]byte, error
 		IsZeroFee: false,
 	})
 
-	ret, usedGas, failed, err := blockchain.NewStateTransition(vmenv, msg, gasPool).TransitionDb()
+	res, err := blockchain.NewStateTransition(vmenv, msg, gasPool).TransitionDb()
 	if err != nil {
 		return nil, fmt.Errorf("%v", err)
 	}
-	if failed {
+	if res.Failed() {
 		return nil, errors.New("transaction failed")
 	}
-	if usedGas == 0 {
+	if res.UsedGas == 0 {
 		return nil, errors.New("usedGas must not be zero")
 	}
 
@@ -169,7 +169,7 @@ func executeWithFee(bc *blockchain.BlockChain, msg types.Message) ([]byte, error
 		return nil, errors.New("originBalance should not equal to balance")
 	}
 
-	return ret, nil
+	return res.ReturnData, nil
 }
 
 func TestStateTransition_TransitionDb_noFee(t *testing.T) {
