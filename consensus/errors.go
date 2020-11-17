8802bbbb3a03cd757d016744f28240e946c48659
lewtran <lewtran@outlook.com>
2020-11-17 12:13:12 +0700

    Remove unused comment

--- consensus/errors.go
+++ consensus/errors.go
@@ -6,15 +6,13 @@ import (
 )
 
 var (
-	// Look like not used (14/11/2020)
-	//ErrInvalidProposalSignature = errors.New("error invalid proposal signature")
 	ErrInvalidProposalPOLRound  = errors.New("error invalid proposal POL round")
 	ErrAddingVote               = errors.New("error adding vote")
 	ErrVoteHeightMismatch       = errors.New("error vote height mismatch")
 	ErrNegativeHeight           = errors.New("negative Height")
 	ErrNegativeRound            = errors.New("negative Round")
 	ErrNegativeIndex            = errors.New("negative Index")
-	ErrNegativeLastCommitRound  = errors.New("negative Index")
+	ErrNegativeLastCommitRound  = errors.New("negative LastCommitRound")
 	ErrNegativeProposalPOLRound = errors.New("negative ProposalPOLRound")
 	ErrEmptyProposalPOL         = errors.New("empty ProposalPOL bit array")
 	ErrInvalidMsgType           = errors.New("invalid message Type")
