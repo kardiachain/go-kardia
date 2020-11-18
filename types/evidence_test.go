c61dfc6e13a421630131a3ca63cf9bd996740039
lewtran <lewtran@outlook.com>
2020-11-18 15:53:09 +0700

    Update evidence testunit

---
+++ types/evidence_test.go
@@ -0,0 +1,197 @@
+/*
+ *  Copyright 2020 KardiaChain
+ *  This file is part of the go-kardia library.
+ *
+ *  The go-kardia library is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The go-kardia library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with the go-kardia library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package types
+
+import (
+	"math"
+	"testing"
+	"time"
+
+	"github.com/kardiachain/go-kardiamain/lib/common"
+	"github.com/kardiachain/go-kardiamain/lib/merkle"
+	kproto "github.com/kardiachain/go-kardiamain/proto/kardiachain/types"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/tendermint/tendermint/crypto"
+)
+
+var defaultVoteTime = time.Date(2019, 1, 1, 0, 0, 0, 0, time.UTC)
+
+func TestEvidenceList(t *testing.T) {
+	ev := randomDuplicateVoteEvidence(t)
+	evl := EvidenceList([]Evidence{ev})
+
+	assert.NotNil(t, evl.Hash())
+	assert.True(t, evl.Has(ev))
+	assert.False(t, evl.Has(&DuplicateVoteEvidence{}))
+}
+
+func randomDuplicateVoteEvidence(t *testing.T) *DuplicateVoteEvidence {
+	val := NewMockPV()
+	blockID := createBlockID(common.BytesToHash(merkle.Sum([]byte("blockhash1"))), 1000, common.BytesToHash([]byte("partshash")))
+	blockID2 := createBlockID(common.BytesToHash(merkle.Sum([]byte("blockhash2"))), 1000, common.BytesToHash([]byte("partshash")))
+	const chainID = "mychain"
+	return &DuplicateVoteEvidence{
+		VoteA: makeVote(t, val, chainID, 0, 10, 2, 1, blockID, defaultVoteTime),
+		VoteB: makeVote(t, val, chainID, 0, 10, 2, 1, blockID2, defaultVoteTime.Add(1*time.Minute)),
+	}
+}
+
+func TestDuplicateVoteEvidence(t *testing.T) {
+	const height = uint64(13)
+	ev := NewMockDuplicateVoteEvidence(height, time.Now(), "mock-chain-id")
+	// assert.Equal(t, ev.Hash().Bytes(), merkle.Sum(ev.Bytes()))
+	assert.NotNil(t, ev.String())
+	assert.Equal(t, ev.Height(), height)
+}
+
+func TestDuplicateVoteEvidenceValidation(t *testing.T) {
+	val := NewMockPV()
+	blockID := createBlockID(common.BytesToHash(merkle.Sum([]byte("blockhash1"))), math.MaxInt32, common.BytesToHash(merkle.Sum([]byte("partshash"))))
+	blockID2 := createBlockID(common.BytesToHash(merkle.Sum([]byte("blockhash2"))), math.MaxInt32, common.BytesToHash(merkle.Sum([]byte("partshash"))))
+	const chainID = "mychain"
+
+	testCases := []struct {
+		testName         string
+		malleateEvidence func(*DuplicateVoteEvidence)
+		expectErr        bool
+	}{
+		{"Good DuplicateVoteEvidence", func(ev *DuplicateVoteEvidence) {}, false},
+		{"Nil vote A", func(ev *DuplicateVoteEvidence) { ev.VoteA = nil }, true},
+		{"Nil vote B", func(ev *DuplicateVoteEvidence) { ev.VoteB = nil }, true},
+		{"Nil votes", func(ev *DuplicateVoteEvidence) {
+			ev.VoteA = nil
+			ev.VoteB = nil
+		}, true},
+		{"Invalid vote type", func(ev *DuplicateVoteEvidence) {
+			ev.VoteA = makeVote(t, val, chainID, math.MaxInt32, math.MaxInt64, math.MaxInt32, 0, blockID2, defaultVoteTime)
+		}, true},
+		{"Invalid vote order", func(ev *DuplicateVoteEvidence) {
+			swap := ev.VoteA.Copy()
+			ev.VoteA = ev.VoteB.Copy()
+			ev.VoteB = swap
+		}, true},
+	}
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.testName, func(t *testing.T) {
+			vote1 := makeVote(t, val, chainID, math.MaxInt32, math.MaxInt64, math.MaxInt32, 0x02, blockID, defaultVoteTime)
+			vote2 := makeVote(t, val, chainID, math.MaxInt32, math.MaxInt64, math.MaxInt32, 0x02, blockID2, defaultVoteTime)
+			ev := NewDuplicateVoteEvidence(vote1, vote2)
+			tc.malleateEvidence(ev)
+			assert.Equal(t, tc.expectErr, ev.ValidateBasic() != nil, "Validate Basic had an unexpected result")
+		})
+	}
+}
+
+func TestMockEvidenceValidateBasic(t *testing.T) {
+	goodEvidence := NewMockDuplicateVoteEvidence(uint64(1), time.Now(), "mock-chain-id")
+	assert.Nil(t, goodEvidence.ValidateBasic())
+}
+
+func TestEvidenceProto(t *testing.T) {
+	// -------- Votes --------
+	val := NewMockPV()
+	blockID := createBlockID(common.BytesToHash(merkle.Sum([]byte("blockhash1"))), math.MaxInt32, common.BytesToHash(merkle.Sum([]byte("partshash"))))
+	blockID2 := createBlockID(common.BytesToHash(merkle.Sum([]byte("blockhash2"))), math.MaxInt32, common.BytesToHash(merkle.Sum([]byte("partshash"))))
+	const chainID = "mychain"
+	v := makeVote(t, val, chainID, math.MaxInt32, math.MaxInt64, 1, 0x01, blockID, defaultVoteTime)
+	v2 := makeVote(t, val, chainID, math.MaxInt32, math.MaxInt64, 2, 0x01, blockID2, defaultVoteTime)
+
+	// -------- SignedHeaders --------
+	const height uint64 = 37
+
+	var (
+		header1 = makeHeaderRandom()
+		header2 = makeHeaderRandom()
+	)
+
+	header1.Height = height
+	header1.LastBlockID = blockID
+
+	header2.Height = height
+	header2.LastBlockID = blockID
+
+	tests := []struct {
+		testName     string
+		evidence     Evidence
+		toProtoErr   bool
+		fromProtoErr bool
+	}{
+		{"nil fail", nil, true, true},
+		{"DuplicateVoteEvidence empty fail", &DuplicateVoteEvidence{}, false, true},
+		{"DuplicateVoteEvidence nil voteB", &DuplicateVoteEvidence{VoteA: v, VoteB: nil}, false, true},
+		{"DuplicateVoteEvidence nil voteA", &DuplicateVoteEvidence{VoteA: nil, VoteB: v}, false, true},
+		{"DuplicateVoteEvidence success", &DuplicateVoteEvidence{VoteA: v2, VoteB: v}, false, false},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.testName, func(t *testing.T) {
+			pb, err := EvidenceToProto(tt.evidence)
+			if tt.toProtoErr {
+				assert.Error(t, err, tt.testName)
+				return
+			}
+			assert.NoError(t, err, tt.testName)
+
+			evi, err := EvidenceFromProto(pb)
+			if tt.fromProtoErr {
+				assert.Error(t, err, tt.testName)
+				return
+			}
+			require.Equal(t, tt.evidence, evi, tt.testName)
+		})
+	}
+}
+
+func makeVote(t *testing.T, val PrivValidator, chainID string, valIndex uint32, height uint64, round uint32, step int, blockID BlockID, time time.Time) *Vote {
+	address := val.GetAddress()
+	v := &Vote{
+		ValidatorAddress: address,
+		ValidatorIndex:   valIndex,
+		Height:           height,
+		Round:            round,
+		Type:             kproto.SignedMsgType(step),
+		BlockID:          blockID,
+		Timestamp:        time,
+	}
+
+	vpb := v.ToProto()
+	err := val.SignVote(chainID, vpb)
+	if err != nil {
+		panic(err)
+	}
+	v.Signature = vpb.Signature
+	return v
+}
+
+func makeHeaderRandom() *Header {
+	return &Header{
+		Height:             uint64(common.RandUint16()) + 1,
+		Time:               time.Now(),
+		LastBlockID:        makeBlockIDRandom(),
+		LastCommitHash:     common.BytesToHash(crypto.CRandBytes(merkle.KaiHashSize)),
+		ValidatorsHash:     common.BytesToHash(crypto.CRandBytes(merkle.KaiHashSize)),
+		NextValidatorsHash: common.BytesToHash(crypto.CRandBytes(merkle.KaiHashSize)),
+		ConsensusHash:      common.BytesToHash(crypto.CRandBytes(merkle.KaiHashSize)),
+		AppHash:            common.BytesToHash(crypto.CRandBytes(merkle.KaiHashSize)),
+		EvidenceHash:       common.BytesToHash(crypto.CRandBytes(merkle.KaiHashSize)),
+		ProposerAddress:    common.BytesToAddress(crypto.CRandBytes(crypto.AddressSize)),
+	}
+}
