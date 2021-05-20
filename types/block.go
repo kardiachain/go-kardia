2148cf24c6423be5ada3c2e45523e2ab6cac994e
Lew Tran <lewtran@outlook.com>
2021-05-20 10:17:23 +0700

    add err check for rlp encode

--- types/block.go
+++ types/block.go
@@ -236,7 +236,9 @@ func (b *Block) Body() *Body {
 
 func rlpHash(x interface{}) (h common.Hash) {
 	hw := sha3.NewKeccak256()
-	rlp.Encode(hw, x)
+	if err := rlp.Encode(hw, x); err != nil {
+		return common.Hash{}
+	}
 	hw.Sum(h[:0])
 	return h
 }
@@ -455,7 +457,9 @@ func (b *Block) Size() common.StorageSize {
 		return size.(common.StorageSize)
 	}
 	c := writeCounter(0)
-	rlp.Encode(&c, b)
+	if err := rlp.Encode(&c, b); err != nil {
+		return 0
+	}
 	b.size.Store(common.StorageSize(c))
 	return common.StorageSize(c)
 }
