f1ee9e0f9c3f5fa896502ba4bbdcdbdd2c6bb8d2
trinhdn97 <trinhdn@protonmail.com>
2021-06-01 11:17:14 +0700

    Remove unused code and configs

--- configs/const.go
+++ configs/const.go
@@ -20,11 +20,8 @@ package configs
 
 import "errors"
 
-// All const related to cross-chain demos including coin exchange and candidate exchange
-// this will be dynamic and removed when run on production
 const (
 	// constants related to account to call smc
-	KardiaAccountToCallSmc = "0xBA30505351c17F4c818d94a990eDeD95e166474b"
 	KardiaPrivKeyToCallSmc = "ae1a52546294bed6e734185775dbc84009de00bdf51b709471e2415c31ceeed7"
 
 	// constants related to rate & addOrder function from smc
@@ -33,34 +30,18 @@ const (
 	NEOSymbol = "NEO"
 
 	// constants related to candidate exchange, Kardia part
-	KardiaCandidateExchangeSmcAddress     = "0x00000000000000000000000000000000736D6338"
-	KardiaPrivateChainCandidateSmcAddress = "0x00000000000000000000000000000000736D6337"
-	KardiaPermissionSmcAddress            = "0x00000000000000000000000000000000736D6336"
-	KardiaForwardRequestFunction          = "forwardRequest"
-	KardiaForwardResponseFunction         = "forwardResponse"
-	KardiaForwardResponseFields           = 4
-	KardiaForwardResponseEmailIndex       = 0
-	KardiaForwardResponseResponseIndex    = 1
-	KardiaForwardResponseFromOrgIndex     = 2
-	KardiaForwardResponseToOrgIndex       = 3
-	KardiaForwardRequestFields            = 3
-	KardiaForwardRequestEmailIndex        = 0
-	KardiaForwardRequestFromOrgIndex      = 1
-	KardiaForwardRequestToOrgIndex        = 2
-
-	// constants related to candidate exchange, private chain part
-	PrivateChainCandidateDBSmcIndex                     = 5
-	PrivateChainCandidateRequestCompletedFields         = 4
-	PrivateChainCandidateRequestCompletedFromOrgIDIndex = 0
-	PrivateChainCandidateRequestCompletedToOrgIDIndex   = 1
-	PrivateChainCandidateRequestCompletedEmailIndex     = 2
-	PrivateChainCandidateRequestCompletedContentIndex   = 3
-	PrivateChainRequestInfoFunction                     = "requestCandidateInfo"
-	PrivateChainCompleteRequestFunction                 = "completeRequest"
-	PrivateChainCandidateRequestFields                  = 3
-	PrivateChainCandidateRequestEmailIndex              = 0
-	PrivateChainCandidateRequestFromOrgIndex            = 1
-	PrivateChainCandidateRequestToOrgIndex              = 2
+	KardiaCandidateExchangeSmcAddress  = "0x00000000000000000000000000000000736D6338"
+	KardiaForwardRequestFunction       = "forwardRequest"
+	KardiaForwardResponseFunction      = "forwardResponse"
+	KardiaForwardResponseFields        = 4
+	KardiaForwardResponseEmailIndex    = 0
+	KardiaForwardResponseResponseIndex = 1
+	KardiaForwardResponseFromOrgIndex  = 2
+	KardiaForwardResponseToOrgIndex    = 3
+	KardiaForwardRequestFields         = 3
+	KardiaForwardRequestEmailIndex     = 0
+	KardiaForwardRequestFromOrgIndex   = 1
+	KardiaForwardRequestToOrgIndex     = 2
 
 	// default value for 0mq
 	DefaultSubscribedEndpoint = "tcp://127.0.0.1:5555"
@@ -70,7 +51,7 @@ const (
 	DefaultTimeOutForStaticCall = 20
 
 	// default params for configs
-	DefaultBcReactorServiceName = "BCREACTOR"
+	DefaultBcReactorServiceName = "BCR"
 )
 
 var (
