eaf9b4290884f6c7e12c46d69fb17864f93359f8
lewtran <lewtran@outlook.com>
2021-12-21 14:02:34 +0700

    bumb version

--- configs/version.go
+++ configs/version.go
@@ -23,8 +23,8 @@ import (
 )
 
 const (
-	VersionMajor        = 1        // Major version component of the current release
-	VersionMinor        = 5        // Minor version component of the current release
+	VersionMajor        = 2        // Major version component of the current release
+	VersionMinor        = 0        // Minor version component of the current release
 	VersionPatch        = 0        // Patch version component of the current release
 	VersionMeta  string = "stable" // Version metadata to append to the version string
 )
