161d9ea1a30a5775dac5b7a4041abda89c84de41
lewtran <lewtran@outlook.com>
2021-12-27 14:19:20 +0700

    fix transaction arg

--- internal/kaiapi/transaction_args.go
+++ internal/kaiapi/transaction_args.go
@@ -63,10 +63,10 @@ type TransactionArgs struct {
 
 // from retrieves the transaction sender address.
 func (arg *TransactionArgs) from() common.Address {
-	if arg.From != nil {
-		return *arg.From
+	if arg.From == nil {
+		return common.Address{}
 	}
-	return configs.GenesisDeployerAddr
+	return *arg.From
 }
 
 // data retrieves the transaction calldata. Input field is preferred.
