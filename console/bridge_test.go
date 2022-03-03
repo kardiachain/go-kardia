b14699f2a066d2268ba292dfc0f2ade320ee740f
trinhdn97 <trinhdn@protonmail.com>
2022-03-03 18:31:56 +0700

    Implement console package

---
+++ console/bridge_test.go
@@ -0,0 +1,32 @@
+package console
+
+import (
+	"testing"
+
+	"github.com/dop251/goja"
+	"github.com/kardiachain/go-kardia/internal/jsre"
+)
+
+// TestUndefinedAsParam ensures that personal functions can receive
+// `undefined` as a parameter.
+func TestUndefinedAsParam(t *testing.T) {
+	b := bridge{}
+	call := jsre.Call{}
+	call.Arguments = []goja.Value{goja.Undefined()}
+
+	b.UnlockAccount(call)
+	b.Sign(call)
+	b.Sleep(call)
+}
+
+// TestNullAsParam ensures that personal functions can receive
+// `null` as a parameter.
+func TestNullAsParam(t *testing.T) {
+	b := bridge{}
+	call := jsre.Call{}
+	call.Arguments = []goja.Value{goja.Null()}
+
+	b.UnlockAccount(call)
+	b.Sign(call)
+	b.Sleep(call)
+}
